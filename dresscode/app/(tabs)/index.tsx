2a060d1e74474ca46618818b7d7b347226610d54
vjz3qz <vjz3qz@virginia.edu>
2024-08-30 18:07:51 -0400

    add tabs for later

--- dresscode/app/(tabs)/index.tsx
+++ dresscode/app/(tabs)/index.tsx
@@ -36,3 +36,72 @@ export default function ExploreScreen() {
     </View>
   );
 }
+
+// TABS
+
+// import React from "react";
+// import Swiper from "react-native-deck-swiper";
+// import { City, Filters, CardItem } from "@/components";
+// import styles from "@/assets/styles";
+// import DEMO from "@/assets/data/demo";
+// import { View, useWindowDimensions } from "react-native";
+// import { TabView, SceneMap } from "react-native-tab-view";
+
+// const FirstRoute = () => (
+//   <View style={styles.containerHome}>
+//     <View style={styles.top}>
+//       <City />
+//       <Filters />
+//     </View>
+//     <View>
+//       <Swiper
+//         cards={DEMO}
+//         infinite
+//         renderCard={(item) => (
+//           <CardItem
+//             image={item.image}
+//             name={item.name}
+//             description={item.description}
+//             price={item.price}
+//           />
+//         )}
+//         verticalSwipe={false}
+//         onSwipedAll={() => {
+//           console.log("All cards swiped!");
+//         }}
+//         cardIndex={0}
+//         backgroundColor={"#4FD0E9"}
+//         stackSize={3}
+//       />
+//     </View>
+//   </View>
+// );
+
+// const SecondRoute = () => (
+//   <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+// );
+
+// const renderScene = SceneMap({
+//   first: FirstRoute,
+//   second: SecondRoute,
+// });
+
+// export default function ExploreScreen() {
+//   const layout = useWindowDimensions();
+
+//   const [index, setIndex] = React.useState(0);
+//   const [routes] = React.useState([
+//     { key: "first", title: "My Closet" },
+//     { key: "second", title: "Shop" },
+//   ]);
+
+//   return (
+//     <TabView
+//       navigationState={{ index, routes }}
+//       renderScene={renderScene}
+//       onIndexChange={setIndex}
+//       initialLayout={{ width: layout.width }}
+//       swipeEnabled={false}
+//     />
+//   );
+// }
