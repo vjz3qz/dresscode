7d6924cf8b11665b0ac66fd670caf58f333a5256
vjz3qz <vjz3qz@virginia.edu>
2024-08-30 16:25:05 -0400

    at

--- dresscode/app/(tabs)/index.tsx
+++ dresscode/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { View, ImageBackground } from "react-native";
 import Swiper from "react-native-deck-swiper";
-import { City, Filters, CardItem } from "../../components";
-import styles from "../../assets/styles";
-import DEMO from "../../assets/data/demo";
+import { City, Filters, CardItem } from "@/components";
+import styles from "@/assets/styles";
+import DEMO from "@/assets/data/demo";
 
 export default function ExploreScreen() {
   return (
