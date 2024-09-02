98d86b94644aad1db57e2020efcf8696fa629bd7
vjz3qz <vjz3qz@virginia.edu>
2024-09-01 23:15:28 -0400

    add tabs

--- client/app/(tabs)/wardrobe.tsx
+++ client/app/(tabs)/wardrobe.tsx
@@ -1,35 +1,81 @@
-import Camera from "@/components/Camera";
-import ComingSoonScreen from "@/components/ComingSoon";
+import UploadButton from "@/ui/UploadButton";
 import React, { useState } from "react";
-import { Button, View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer"; // Add this import
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+} from "react-native";
+import Camera from "@/components/Camera";
+import ImageViewer from "@/components/ImageViewer";
+
+const tabs = [{ name: "Items" }, { name: "Outfits" }, { name: "Looks" }];
 
 export default function WardrobeScreen() {
   const [cameraOpen, setCameraOpen] = useState<boolean>(false);
   const [imageName, setImageName] = useState<string | null>(null);
+  const [value, setValue] = useState(0);
+
   return cameraOpen ? (
     <Camera
       exitCamera={() => setCameraOpen(false)}
       setImageName={setImageName}
     />
   ) : imageName ? (
-    <ImageViewer imageName={imageName} /> // Correct the conditional rendering syntax
+    <ImageViewer imageName={imageName} />
   ) : (
-    <View style={styles.container}>
-      <Button
-        onPress={() => setCameraOpen(true)}
-        title="Open Camera"
-        color="#841584"
-        accessibilityLabel="Open Camera"
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        {tabs.map((item, index) => {
+          const isActive = index === value;
+
+          return (
+            <View key={item.name} style={{ flex: 1 }}>
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  setValue(index);
+                }}
+              >
+                <View
+                  style={[
+                    styles.item,
+                    isActive && { backgroundColor: "#e0e7ff" },
+                  ]}
+                >
+                  <Text style={[styles.text, isActive && { color: "#4338ca" }]}>
+                    {item.name}
+                  </Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          );
+        })}
+      </View>
+      <UploadButton onPress={() => setCameraOpen(true)} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: "row",
+    paddingVertical: 24,
+    paddingHorizontal: 12,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  item: {
     justifyContent: "center",
     alignItems: "center",
+    paddingVertical: 10,
+    backgroundColor: "transparent",
+    borderRadius: 6,
+  },
+  text: {
+    fontSize: 13,
+    fontWeight: "600",
+    color: "#6b7280",
   },
 });
