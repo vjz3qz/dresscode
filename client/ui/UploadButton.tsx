83fd3605c8a0866685de86da78386c9da9e8b9b7
vjz3qz <vjz3qz@virginia.edu>
2024-09-01 23:15:05 -0400

    create upload button

---
+++ client/ui/UploadButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { FAB } from "react-native-paper";
+
+export default function UploadButton({ onPress }: { onPress: () => void }) {
+  return <FAB icon="plus" style={styles.fab} onPress={onPress} />;
+}
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
