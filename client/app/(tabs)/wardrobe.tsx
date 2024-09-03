53ebcf1f0d495b0e6eda121782328e8dcb1e396f
vjz3qz <vjz3qz@virginia.edu>
2024-09-03 16:00:54 -0400

    good view

--- client/app/(tabs)/wardrobe.tsx
+++ client/app/(tabs)/wardrobe.tsx
@@ -6,12 +6,31 @@ import {
   Text,
   TouchableWithoutFeedback,
   SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import Gallery from "react-native-awesome-gallery";
 import Camera from "@/components/Camera";
 import ImageViewer from "@/components/ImageViewer";
 import { supabase } from "@/utils/Supabase";
 import axios from "axios";
+import { Image } from "expo-image";
+
+const { height } = Dimensions.get("window");
+
+const images = [
+  "https://picsum.photos/id/1015/600/400",
+  "https://picsum.photos/id/1025/600/400",
+  "https://picsum.photos/id/1035/600/400",
+  "https://picsum.photos/id/1045/600/400",
+  "https://picsum.photos/id/1055/600/400",
+  "https://picsum.photos/id/1065/600/400",
+  "https://picsum.photos/id/1075/600/400",
+  "https://picsum.photos/id/1076/600/400",
+  "https://picsum.photos/id/1077/600/400",
+  "https://picsum.photos/id/1078/600/400",
+];
 
 const tabs = [
   { name: "Items", tableName: "items" },
@@ -24,6 +43,9 @@ export default function WardrobeScreen() {
   const [imageName, setImageName] = useState<string | null>(null);
   const [value, setValue] = useState(0);
   const [data, setData] = useState<string[]>([]);
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
+    null
+  );
 
   const fetchImageUrl = async (imageName: string) => {
     if (!imageName) {
@@ -71,6 +93,21 @@ export default function WardrobeScreen() {
     fetchItems();
   }, [value]);
 
+  if (selectedImageIndex !== null) {
+    return (
+      <View style={{ flex: 1 }}>
+        {/* X button to close the gallery */}
+        <TouchableOpacity
+          style={styles.closeButton}
+          onPress={() => setSelectedImageIndex(null)}
+        >
+          <Text style={styles.closeButtonText}>X</Text>
+        </TouchableOpacity>
+        <Gallery initialIndex={selectedImageIndex} data={images} />
+      </View>
+    );
+  }
+
   return cameraOpen ? (
     <Camera
       exitCamera={() => setCameraOpen(false)}
@@ -80,41 +117,43 @@ export default function WardrobeScreen() {
     <ImageViewer imageName={imageName} />
   ) : (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
+      <View style={[styles.container]}>
         {tabs.map((item, index) => {
           const isActive = index === value;
 
           return (
-            <View key={item.name} style={{ flex: 1 }}>
-              <TouchableWithoutFeedback
-                onPress={() => {
-                  setValue(index);
-                }}
+            <TouchableWithoutFeedback
+              key={item.name}
+              onPress={() => {
+                setValue(index);
+              }}
+            >
+              <View
+                style={[
+                  styles.item,
+                  isActive && { backgroundColor: "#e0e7ff" },
+                ]}
               >
-                <View
-                  style={[
-                    styles.item,
-                    isActive && { backgroundColor: "#e0e7ff" },
-                  ]}
-                >
-                  <Text style={[styles.text, isActive && { color: "#4338ca" }]}>
-                    {item.name}
-                  </Text>
-                </View>
-              </TouchableWithoutFeedback>
-            </View>
+                <Text style={[styles.text, isActive && { color: "#4338ca" }]}>
+                  {item.name}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
           );
         })}
       </View>
-
-      <Gallery
-        style={{ flex: 5 }} // Ensure the gallery takes up the rest of the space
-        // data={data}
-        data={[
-          "https://cdn.pixabay.com/photo/2021/05/28/13/04/planet-6290805_1280.jpg",
-          "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-        ]}
-      />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.gridContainer}>
+          {images.map((uri, index) => (
+            <TouchableWithoutFeedback
+              key={uri}
+              onPress={() => setSelectedImageIndex(index)}
+            >
+              <Image source={uri} style={styles.image} />
+            </TouchableWithoutFeedback>
+          ))}
+        </View>
+      </ScrollView>
       <UploadButton onPress={() => setCameraOpen(true)} />
     </SafeAreaView>
   );
@@ -125,9 +164,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingVertical: 24,
     paddingHorizontal: 12,
-    flexGrow: 1,
-    flexShrink: 1,
-    flexBasis: 0,
+    justifyContent: "space-around",
   },
   item: {
     justifyContent: "center",
@@ -135,10 +172,41 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     backgroundColor: "transparent",
     borderRadius: 6,
+    width: 100, // Fixed width for each tab
   },
   text: {
     fontSize: 13,
     fontWeight: "600",
     color: "#6b7280",
+  },
+  debugBorder: {
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  gridContainer: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  image: {
+    width: "50%",
+    height: (height / 8) * 2,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+    zIndex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 10,
+    borderRadius: 20,
+  },
+  closeButtonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
