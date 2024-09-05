7d92b7c91093552abf9e36d02cb971c80d28d6b5
vjz3qz <vjz3qz@virginia.edu>
2024-09-05 00:08:02 -0400

    create

---
+++ client/api/FetchImageUrl.tsx
@@ -0,0 +1,40 @@
+import { supabase } from "@/utils/Supabase";
+import axios from "axios";
+
+export async function fetchImageUrl(imageName: string) {
+  if (!imageName) {
+    return;
+  }
+
+  try {
+    const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+    const response = await axios.get(
+      `${BACKEND_URL}/get-image-url/${imageName}`
+    );
+
+    return response.data["result"]["url"];
+  } catch (error) {
+    throw new Error(`Error fetching image URL: ${error}`);
+    console.error("Error fetching image URL:", error);
+  }
+}
+
+export async function fetchAllItemImageUrls(tableName: string) {
+  let { data, error } = await supabase.from(tableName).select("*");
+
+  if (error) {
+    throw new Error(`Error fetching items: ${error.message}`);
+  }
+  if (!data || data.length === 0) {
+    throw new Error("No data found");
+  }
+  let items: string[] = [];
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i]["s3_key"]) {
+      continue;
+    }
+    const imageUrl = await fetchImageUrl(data[i]["s3_key"]);
+    items.push(imageUrl);
+  }
+  return items;
+}
