09b3457291cbc16b130857e0729bec19e49aeffe
vjz3qz <vjz3qz@virginia.edu>
2024-09-09 17:08:45 -0400

    works

--- client/components/DraggableImage.tsx
+++ client/components/DraggableImage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useImperativeHandle, forwardRef } from "react";
 import { Image, StyleSheet, Dimensions } from "react-native";
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 import Animated, {
@@ -9,67 +9,86 @@ import Animated, {
 
 const { width, height } = Dimensions.get("window");
 
-const DraggableImage = ({
-  imageUri,
-  initialX = width / 2 - 75,
-  initialY = height / 2 - 75,
-  initialSize = 150,
-}: {
+interface DraggableImageProps {
   imageUri: string;
   initialX?: number;
   initialY?: number;
   initialSize?: number;
-}) => {
-  const translateX = useSharedValue(initialX);
-  const translateY = useSharedValue(initialY);
-  const startX = useSharedValue(0);
-  const startY = useSharedValue(0);
+}
 
-  const scale = useSharedValue(1);
-  const savedScale = useSharedValue(1);
+export interface DraggableImageRef {
+  getPositionAndSize: () => { x: number; y: number; size: number };
+}
 
-  const pinchGesture = Gesture.Pinch()
-    .onUpdate((event) => {
-      scale.value = savedScale.value * event.scale;
-    })
-    .onEnd(() => {
-      savedScale.value = scale.value;
+const DraggableImage = forwardRef<DraggableImageRef, DraggableImageProps>(
+  (
+    {
+      imageUri,
+      initialX = width / 2 - 75,
+      initialY = height / 2 - 75,
+      initialSize = 150,
+    },
+    ref
+  ) => {
+    const translateX = useSharedValue(initialX);
+    const translateY = useSharedValue(initialY);
+    const startX = useSharedValue(0);
+    const startY = useSharedValue(0);
+
+    const scale = useSharedValue(1);
+    const savedScale = useSharedValue(1);
+
+    useImperativeHandle(ref, () => ({
+      getPositionAndSize: () => ({
+        x: translateX.value,
+        y: translateY.value,
+        size: initialSize * savedScale.value,
+      }),
+    }));
+
+    const pinchGesture = Gesture.Pinch()
+      .onUpdate((event) => {
+        scale.value = savedScale.value * event.scale;
+      })
+      .onEnd(() => {
+        savedScale.value = scale.value;
+      });
+
+    const panGesture = Gesture.Pan()
+      .onStart(() => {
+        startX.value = translateX.value;
+        startY.value = translateY.value;
+      })
+      .onUpdate((event) => {
+        translateX.value = startX.value + event.translationX;
+        translateY.value = startY.value + event.translationY;
+      })
+      .onEnd(() => {
+        translateX.value = withSpring(translateX.value);
+        translateY.value = withSpring(translateY.value);
+      });
+
+    const combinedGesture = Gesture.Simultaneous(panGesture, pinchGesture);
+
+    const animatedStyle = useAnimatedStyle(() => {
+      return {
+        transform: [
+          { translateX: translateX.value },
+          { translateY: translateY.value },
+          { scale: scale.value },
+        ],
+      };
     });
 
-  const panGesture = Gesture.Pan()
-    .onStart(() => {
-      startX.value = translateX.value;
-      startY.value = translateY.value;
-    })
-    .onUpdate((event) => {
-      translateX.value = startX.value + event.translationX;
-      translateY.value = startY.value + event.translationY;
-    })
-    .onEnd(() => {
-      translateX.value = withSpring(translateX.value);
-      translateY.value = withSpring(translateY.value);
-    });
-
-  const combinedGesture = Gesture.Simultaneous(panGesture, pinchGesture);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
-        { scale: scale.value },
-      ],
-    };
-  });
-
-  return (
-    <GestureDetector gesture={combinedGesture}>
-      <Animated.View style={[styles.box(initialSize), animatedStyle]}>
-        <Image source={{ uri: imageUri }} style={styles.image} />
-      </Animated.View>
-    </GestureDetector>
-  );
-};
+    return (
+      <GestureDetector gesture={combinedGesture}>
+        <Animated.View style={[styles.box(initialSize), animatedStyle]}>
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        </Animated.View>
+      </GestureDetector>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   box: (initialSize: number) => ({
