e568ded6fefae4e9ad3141f05a907ce6642c9ffe
vjz3qz <vjz3qz@virginia.edu>
2024-09-16 14:54:17 -0400

    fetch bulk

--- client/api/FetchLooks.ts
+++ client/api/FetchLooks.ts
@@ -1,6 +1,6 @@
 import { Look, Outfit } from "@/types";
 import { supabase } from "@/utils/Supabase";
-import { fetchImageUrl } from "@/api/FetchImageUrl";
+import { addImageUrls } from "@/api/FetchImageUrl";
 
 // Function to fetch looks from the Supabase table
 export async function fetchLooks(): Promise<Look[]> {
@@ -61,17 +61,8 @@ export async function loadLooksWithOutfits(
   try {
     for (const look of fetchedLooks) {
       const outfits = await fetchOutfitsByLook((look.id || "").toString());
-      look.outfits = outfits as Outfit[];
-
-      // Use Promise.all to fetch all image URLs for this look's outfits
-      await Promise.all(
-        look.outfits.map(async (outfit) => {
-          if (outfit.s3_key) {
-            const url = await fetchImageUrl(outfit.s3_key);
-            outfit.image_url = url;
-          }
-        })
-      );
+      const outfitsWithImages = await addImageUrls(outfits);
+      look.outfits = outfitsWithImages;
     }
 
     return fetchedLooks;
