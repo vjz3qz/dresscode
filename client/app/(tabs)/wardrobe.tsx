d44665a264cc62e27a934561b852fe3c6bc7291e
vjz3qz <vjz3qz@virginia.edu>
2024-11-06 20:40:12 -0500

    update

--- client/app/(tabs)/wardrobe.tsx
+++ client/app/(tabs)/wardrobe.tsx
@@ -1,5 +1,5 @@
 import UploadButton from "@/ui/UploadButton";
-import React, { useEffect, useState } from "react";
+import React, { useState, useCallback } from "react";
 import {
   StyleSheet,
   View,
@@ -10,77 +10,35 @@ import {
   Dimensions,
   Image,
 } from "react-native";
-import { fetchAllImageUrls } from "@/api/FetchImageUrl";
-import { router } from "expo-router";
-import { Item, TableTypes } from "@/types";
+import { Href, router } from "expo-router";
+import { Item, Look, TableTypes } from "@/types";
+import { useSession } from "@/contexts/SessionContext";
+import { Ionicons } from "@expo/vector-icons";
 import Feed from "@/components/Feeds/Feed";
 import LookFeed from "@/components/Feeds/LookFeed";
-import ImageGallery from "@/components/ImageGallery";
-import { useSession } from "@/contexts/SessionContext";
-import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 
 const { height } = Dimensions.get("window");
 
-const tabs = [
+const tabs: { name: string; tableName: keyof TableTypes }[] = [
   { name: "Items", tableName: "items" },
   { name: "Outfits", tableName: "outfits" },
   { name: "Looks", tableName: "looks" },
 ];
 
 export default function WardrobeScreen() {
-  const { session, username, setUsername, avatarUrl, setAvatarUrl, loading } =
-    useSession();
+  const { username, avatarUrl } = useSession();
   const [tabIndex, setTabIndex] = useState(0);
-  const [data, setData] = useState<any[]>([]);
-  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
-    null
-  );
+
+  const handleTabClick = useCallback((index: number) => setTabIndex(index), []);
 
   function onPlusButtonClick() {
-    if (tabs[tabIndex]["tableName"] === "items") {
-      router.push("/wardrobe/new/item");
-    } else if (tabs[tabIndex]["tableName"] === "outfits") {
-      router.push("/wardrobe/new/outfit");
-    } else if (tabs[tabIndex]["tableName"] === "looks") {
-      router.push("/wardrobe/new/look");
-    }
-  }
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      let items: any[] = [];
-      try {
-        if (!session) {
-          return;
-        }
-        items = await fetchAllImageUrls(
-          tabs[tabIndex]["tableName"] as keyof TableTypes,
-          session
-        );
-      } catch (error: any) {
-        console.error("Error fetching items:", error.message);
-        setData([]);
-        return;
-      }
-      setData(items);
+    const paths = {
+      items: "/wardrobe/new/item",
+      outfits: "/wardrobe/new/outfit",
+      looks: "/wardrobe/new/look",
     };
-
-    fetchItems();
-  }, [tabIndex]);
-
-  if (selectedImageIndex !== null) {
-    return (
-      <ImageGallery
-        loading={false}
-        data={data.map((object) => object.image_url)}
-        selectedImageIndex={selectedImageIndex}
-        onClose={() => {
-          setSelectedImageIndex(null);
-        }}
-      />
-    );
+    router.push(paths[tabs[tabIndex].tableName] as Href<string>);
   }
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* Header */}
@@ -91,27 +49,28 @@ export default function WardrobeScreen() {
         />
         <Text style={styles.nameText}>{username || "Name"}</Text>
         <Text style={styles.usernameText}>@{username || "name"}</Text>
-        {/* Welcome Text */}
-        {/* <Text style={styles.welcomeText}>Welcome to Dress Code.</Text> */}
       </View>
+
       {/* Tabs */}
       <View style={styles.tabsContainer}>
-        {tabs.map((item, index) => {
-          const isActive = index === tabIndex;
-          return (
-            <TouchableWithoutFeedback
-              key={item.name}
-              onPress={() => setTabIndex(index)}
+        {tabs.map((item, index) => (
+          <TouchableWithoutFeedback
+            key={item.name}
+            onPress={() => handleTabClick(index)}
+          >
+            <View
+              style={[styles.item, index === tabIndex && styles.activeItem]}
             >
-              <View style={[styles.item, isActive && styles.activeItem]}>
-                <Text style={[styles.text, isActive && styles.activeText]}>
-                  {item.name}
-                </Text>
-              </View>
-            </TouchableWithoutFeedback>
-          );
-        })}
+              <Text
+                style={[styles.text, index === tabIndex && styles.activeText]}
+              >
+                {item.name}
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        ))}
       </View>
+
       {/* Search Bar */}
       <View style={styles.searchContainer}>
         <Ionicons name="search" size={20} color="#6b7280" />
@@ -124,19 +83,21 @@ export default function WardrobeScreen() {
       </View>
 
       {/* Feeds */}
-      {tabs[tabIndex]["tableName"] === "looks" ? (
+      {tabIndex === 2 ? (
         <LookFeed
-          onLookClick={(look: any, index: number) => {
-            router.push(`/wardrobe/view/look-outfits/${look.id}`);
+          onLookClick={(look: Look, index: number) => {
+            if (look.id) router.push(`/wardrobe/view/look/${look.id}` as Href);
           }}
-          tableName={tabs[tabIndex]["tableName"] as keyof TableTypes}
+          tableName={tabs[tabIndex].tableName as keyof TableTypes}
         />
       ) : (
         <Feed
-          onObjectClick={(item: Item, index: number) => {
-            setSelectedImageIndex(index);
-          }}
-          tableName={tabs[tabIndex]["tableName"] as keyof TableTypes}
+          onObjectClick={(item: any, index: number) =>
+            router.push(
+              `/wardrobe/view/${tabs[tabIndex].tableName}/${item.id}` as Href
+            )
+          }
+          tableName={tabs[tabIndex].tableName as keyof TableTypes}
         />
       )}
 
@@ -151,25 +112,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingHorizontal: 16,
     paddingTop: 20,
-  },
-  logo: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: "#F3F4F6", // Placeholder background color
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginLeft: 8,
-    flex: 1,
-    color: "#333",
-  },
-  welcomeText: {
-    fontSize: 14,
-    color: "#6b7280",
-    paddingHorizontal: 8,
-    marginTop: 4,
   },
   searchContainer: {
     flexDirection: "row",
@@ -187,20 +129,9 @@ const styles = StyleSheet.create({
     marginLeft: 8,
     color: "#333",
   },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#333",
-  },
-  viewAll: {
-    fontSize: 14,
-    color: "#6b7280",
-    textDecorationLine: "underline",
-  },
   tabsContainer: {
     flexDirection: "row",
     paddingTop: 12,
-
     paddingHorizontal: 16,
     justifyContent: "space-around",
   },
@@ -208,18 +139,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingVertical: 10,
-    backgroundColor: "transparent",
     borderRadius: 8,
-    width: 100, // Fixed width for each tab
+    width: 100,
   },
   text: {
     fontSize: 14,
     fontWeight: "600",
-    color: "#6b7280", // Neutral gray for inactive text
+    color: "#6b7280",
   },
   activeItem: {
-    backgroundColor: "#EAE6E5", // Light background for active tab
-    borderRadius: 8,
+    backgroundColor: "#EAE6E5",
   },
   activeText: {
     color: "#0f0f0f",
