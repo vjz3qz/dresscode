9f28ebb014146d57a4c8c36fd81cb373890622c9
vjz3qz <vjz3qz@virginia.edu>
2024-11-06 22:45:56 -0500

    add session

--- client/api/SaveOutfit.ts
+++ client/api/SaveOutfit.ts
@@ -1,10 +1,15 @@
 import { Outfit } from "@/types";
 import { supabase } from "@/utils/Supabase";
 import { uploadImageUri } from "@/api/UploadImage";
+import { fetchSession } from "@/utils/FetchSession";
 
 export async function saveOutfit(outfit: Outfit, screenshotUri: string) {
   try {
-    const s3Key = await uploadImageUri(screenshotUri);
+    const session = await fetchSession();
+    if (!session) {
+      throw new Error("No active session");
+    }
+    const s3Key = await uploadImageUri(screenshotUri, session);
 
     // Add the S3 key to the outfit metadata and save the outfit to Supabase
     outfit.s3_key = s3Key;
