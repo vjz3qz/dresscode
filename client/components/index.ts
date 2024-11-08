dc2301336444b0549755acd422ff3ad5679784f3
vjz3qz <vjz3qz@virginia.edu>
2024-11-08 18:16:55 -0500

    remove

--- client/components/index.ts
+++ client/components/index.ts
@@ -2,5 +2,3 @@ export { default as CardItem } from "./CardItem";
 export { default as City } from "./City";
 export { default as Filters } from "./Filters";
 export { default as Icon } from "./Icon";
-export { default as Message } from "./Message";
-export { default as ProfileItem } from "./ProfileItem";
--- client/types.ts
+++ client/types.ts
@@ -12,29 +12,6 @@ export type IconT = {
   style?: any;
 };
 
-export type MessageT = {
-  image: any;
-  lastMessage: string;
-  name: string;
-};
-
-export type ProfileItemT = {
-  age?: string;
-  info1?: string;
-  info2?: string;
-  info3?: string;
-  info4?: string;
-  location?: string;
-  matches: string;
-  name: string;
-};
-
-export type TabBarIconT = {
-  focused: boolean;
-  iconName: any;
-  text: string;
-};
-
 export type DataT = {
   id: number;
   name: string;
