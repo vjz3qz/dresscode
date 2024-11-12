4e65ddb0cdf612faa533538bddd901e8190fd970
vjz3qz <vjz3qz@virginia.edu>
2024-11-11 19:10:09 -0500

    add metadata bottom sheet form

--- client/app/wardrobe/new/look.tsx
+++ client/app/wardrobe/new/look.tsx
@@ -1,64 +1,134 @@
 import React, { useRef, useState } from "react";
-import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { Look, Outfit } from "@/types";
 import SelectFeed from "@/components/Feeds/SelectFeed";
 import { router } from "expo-router";
 import { saveLook } from "@/api/SaveLook";
+import BottomSheet, { BottomSheetMethods } from "@devvie/bottom-sheet";
 
 export default function NewLookScreen() {
   const [selectedOutfits, setSelectedOutfits] = useState<Outfit[]>([]);
+  const [metadata, setMetadata] = useState({
+    name: "New Look",
+    description: "A new look",
+  });
+  const sheetRef = useRef<BottomSheetMethods>(null);
 
   function onSelect(selectedOutfits: Outfit[]) {
     setSelectedOutfits(selectedOutfits);
   }
+
   async function save() {
-    console.log("Saving look with outfits:", selectedOutfits);
     const newLook: Look = {
-      name: "New Look",
-      description: "A new look",
+      name: metadata.name,
+      description: metadata.description,
     };
     await saveLook(newLook, selectedOutfits);
     router.dismiss();
   }
 
+  function toggleView() {
+    // setShowMetadataForm(!showMetadataForm);() => sheetRef.current?.open()
+    sheetRef.current?.open();
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <TouchableOpacity style={styles.saveButton} onPress={save}>
-        <Text style={styles.buttonText}>Save</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={toggleView} style={styles.iconButton}>
+          <Text style={styles.buttonText}>{"Edit Metadata"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={save} style={styles.iconButton}>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+      </View>
       <View style={styles.feedContainer}>
         <SelectFeed tableName="outfits" onSelect={onSelect} />
       </View>
-    </View>
+      <BottomSheet
+        ref={sheetRef}
+        style={{
+          backgroundColor: "white",
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+        }}
+      >
+        <ScrollView style={styles.metadataContainer}>
+          <Text style={styles.label}>Look Name</Text>
+          <TextInput
+            style={styles.input}
+            value={metadata.name}
+            onChangeText={(text) => setMetadata({ ...metadata, name: text })}
+            placeholder="Enter name"
+            placeholderTextColor="#b0b0b0"
+          />
+
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={styles.input}
+            value={metadata.description}
+            onChangeText={(text) =>
+              setMetadata({ ...metadata, description: text })
+            }
+            placeholder="Enter description"
+            placeholderTextColor="#b0b0b0"
+          />
+        </ScrollView>
+      </BottomSheet>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  addButton: {
-    position: "absolute",
-    top: 50,
-    left: 20,
-    zIndex: 1,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    padding: 10,
-    borderRadius: 20,
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
   },
-  saveButton: {
-    position: "absolute",
-    top: 50,
-    right: 20,
-    zIndex: 1,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    padding: 10,
-    borderRadius: 20,
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingTop: 20,
+  },
+  iconButton: {
+    backgroundColor: "#333",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
   },
   buttonText: {
-    color: "white",
-    fontSize: 18,
+    color: "#fff",
+    fontSize: 16,
     fontWeight: "bold",
   },
   feedContainer: {
     flex: 1,
-    paddingTop: 100, // Add padding above the SelectFeed
+    paddingTop: 20,
+  },
+  metadataContainer: {
+    padding: 16,
+  },
+  label: {
+    fontSize: 18,
+    color: "#6b7280",
+    marginTop: 20,
+  },
+  input: {
+    borderColor: "#e0e0e0",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    backgroundColor: "#fff",
+    marginBottom: 20,
   },
 });
