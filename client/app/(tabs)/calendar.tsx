4416149c65fa9590873bdfe7d15b08e5acf7608d
vjz3qz <vjz3qz@virginia.edu>
2025-01-01 20:20:08 -0500

    created a calendar list

--- client/app/(tabs)/calendar.tsx
+++ client/app/(tabs)/calendar.tsx
@@ -1,6 +1,31 @@
 import ComingSoonScreen from "@/components/ComingSoon";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import React from "react";
+import { SafeAreaView, View, StyleSheet } from "react-native";
 
-export default function CalendarScreen() {
-  return <ComingSoonScreen />;
+interface Day {
+  year: number;
+  month: number;
+  day: number;
+  timestamp: number;
+  dateString: string;
 }
+export default function CalendarScreen() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <CalendarList
+        style={styles.calendar}
+        onDayPress={(day: Day) => {
+          console.log("selected day", day);
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, // Ensure SafeAreaView takes the full screen
+  },
+  calendar: {},
+});
