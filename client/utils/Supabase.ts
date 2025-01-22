279e7011264975eb502356c69edfd013b0ac00ee
vjz3qz <vjz3qz@virginia.edu>
2025-01-21 23:06:54 -0500

    add throw errors if not found variables

--- client/utils/Supabase.ts
+++ client/utils/Supabase.ts
@@ -3,6 +3,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL ?? ""; // Provide a default value if supabaseUrl is undefined
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY ?? ""; // Provide a default value if supabaseKey is undefined
+
+// console.log("supabaseUrl:", supabaseUrl);
+// console.log("supabaseKey:", supabaseKey);
+if (!supabaseUrl) {
+  throw new Error("Missing env.EXPO_PUBLIC_SUPABASE_PROJECT_URL");
+}
+if (!supabaseKey) {
+  throw new Error("Missing env.EXPO_PUBLIC_SUPABASE_KEY");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     storage: AsyncStorage,
