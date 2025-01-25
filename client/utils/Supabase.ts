7376d414fce907c1abb48d9ec8bc77e5562f3f35
vjz3qz <vjz3qz@virginia.edu>
2025-01-24 21:45:48 -0500

    rm log

--- client/utils/Supabase.ts
+++ client/utils/Supabase.ts
@@ -4,8 +4,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_PROJECT_URL ?? ""; // Provide a default value if supabaseUrl is undefined
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY ?? ""; // Provide a default value if supabaseKey is undefined
 
-console.log("supabaseUrl:", supabaseUrl);
-console.log("supabaseKey:", supabaseKey);
+// console.log("supabaseUrl:", supabaseUrl);
+// console.log("supabaseKey:", supabaseKey);
 if (!supabaseUrl) {
   throw new Error("Missing env.EXPO_PUBLIC_SUPABASE_PROJECT_URL");
 }
